92d47e9e7bc17c68253c515304070e364c2b616b
gokmen.yilmaz <gokmen1977@gmail.com>
2021-03-17 09:08:29 +0300

    uu

---
+++ WpfApp1/DokumAlasimSinirRenkConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class DokumAlasimSinirRenkConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string deger = (string)value;
+
+            var renk = deger.Length > 0 ? Colors.Red : Colors.Green;
+
+            return renk;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
