770aba870ac6455e05f4845b9e19fdaec1432ae6
gokmen.yilmaz <gokmen1977@gmail.com>
2021-03-17 09:14:25 +0300

    uu

--- WpfApp1/DokumAlasimSinirRenkConverter.cs
+++ WpfApp1/DokumAlasimSinirRenkConverter.cs
@@ -15,7 +15,7 @@ namespace WpfApp1
         {
             string deger = (string)value;
 
-            var renk = deger.Length > 0 ? Colors.Red : Colors.Green;
+            var renk = deger.Length > 0 ? "Red" : "Green";
 
             return renk;
 
