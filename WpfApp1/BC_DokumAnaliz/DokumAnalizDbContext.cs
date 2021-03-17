3d3fad9a65b661ef1b6961d2970239bdc38bbe80
gokmen.yilmaz <gokmen1977@gmail.com>
2021-03-17 07:54:31 +0300

    uu

---
+++ WpfApp1/BC_DokumAnaliz/DokumAnalizDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.BC_DokumKalite;
+
+namespace WpfApp1.BC_DokumAnaliz
+{
+    public class DokumAnalizDbContext:DbContext
+    {
+        public DbSet<DokumAlasimSinir> DokumAlasimSinirlari { get; set; }
+
+        public DbSet<DokumAnalizSonuc> DokumAnalizSonuclari { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cnnStr = "server=.,database=pandapdb;user id=sa;password=Ankara!06";
+            optionsBuilder.UseSqlServer(cnnStr);
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ WpfApp1/BC_DokumAnaliz/DokumAnalizRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.BC_DokumKalite;
+
+namespace WpfApp1.BC_DokumAnaliz
+{
+    public class DokumAnalizRepository
+    {
+        DokumAnalizDbContext dc = new DokumAnalizDbContext();
+       
+        public List<DokumAlasimSinir> DokumAnalizSinirlariGetir()
+        {
+            var sonuc=dc.DokumAlasimSinirlari.ToList();
+
+            return sonuc;
+        }
+
+
+        public List<DokumAnalizSonuc> DokumAnalizSonuclariGetir(DateTime date)
+        {
+            var sonuc = dc.DokumAnalizSonuclari
+                .Where(c => c.TarihSaat.Date == date)
+                .ToList();
+
+            return sonuc;
+        }
+    }
+}
